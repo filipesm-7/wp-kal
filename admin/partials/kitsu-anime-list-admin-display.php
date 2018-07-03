b2d3eda3ae3dd461af0c9518ff81eba8736eca9f
Filipe Mendonça <filipesm.7@gmail.com>
2018-07-03 22:03:26 +0100

    Added support to internationalization and changed hardcoded value for KAL class constant

--- admin/partials/kitsu-anime-list-admin-display.php
+++ admin/partials/kitsu-anime-list-admin-display.php
@@ -1,23 +1,22 @@
 <?php
-//TODO: change to another place if possible
-$max_items = 5;
+
+//get plugin options
 $options = get_option($this->plugin_name);
 
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="wrap">
-    <h2>Kitsu Anime List customization</h2>
+    <h2><?php _e('Kitsu Anime List customization', $this->plugin_name); ?></h2>
 
     <form method="post" name="kal_options" action="options.php">
         <?php settings_fields($this->plugin_name); ?>
 
         <fieldset>
             <label for="<?php echo $this->plugin_name; ?>-kal">
-                <!-- TODO internationalization -->
-                <span><?php esc_attr_e('Items per page', $this->plugin_name); ?></span>
+                <span><?php _e('Items per page', $this->plugin_name); ?></span>
                 <select id="<?php echo $this->plugin_name; ?>-kal" name="<?php echo $this->plugin_name; ?>[items_per_page]">
-                    <?php for ($i=1; $i <= $max_items; $i++): ?>
+                    <?php for ($i=1; $i <= $this::MAX_ITEMS_SHOWN; $i++): ?>
                         <option
                             value="<?php echo $i; ?>"
                             <?php if($i == $options['items_per_page']) echo 'selected' ?>
@@ -29,7 +28,6 @@ $options = get_option($this->plugin_name);
             </label>
         </fieldset>
 
-        <!-- TODO internationalization -->
-        <?php submit_button('Save all changes', 'primary', 'submit', TRUE); ?>
+        <?php submit_button(__('Save all changes', $this->plugin_name), 'primary', 'submit', TRUE); ?>
     </form>
 </div>
