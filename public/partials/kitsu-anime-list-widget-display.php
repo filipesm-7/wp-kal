3f56bdcbecf3c5af9a5c97d32f3f65f2881058ad
Filipe Mendonça <filipesm.7@gmail.com>
2018-07-06 19:50:41 +0100

    Templating done on widget display

--- public/partials/kitsu-anime-list-widget-display.php
+++ public/partials/kitsu-anime-list-widget-display.php
@@ -2,27 +2,19 @@
     <h2 class="widget-title"><?php echo $instance['title']; ?></h2>
 
     <ul>
-        <li class="item">
-            <p>
-                <a href="https://kitsu.io/anime/spice-and-wolf">
-                    <img class="kitsu-anime-list-image" src="https://media.kitsu.io/anime/poster_images/3936/tiny.jpg?1419000387" alt="Spice and Wolf" />
-                </a>
-            </p>
-            <a href="" class="title">Fullmetal Alchemist: Brotherhood</a>
-            <span>TV, 64 episodes, finished</span>
-            <span>87.02 rating</span>
-        </li>
-        <li class="item">
-            <p>
-                <a href="https://kitsu.io/anime/spice-and-wolf">
-                    <img class="kitsu-anime-list-image" src="https://media.kitsu.io/anime/poster_images/12268/tiny.jpg?1491106234" alt="Spice and Wolf" />
-                </a>
-            </p>
-
-            <a href="" class="title">My Hero Academia Season 2</a>
-            <span>TV, 25 episodes, finished</span>
-            <span>85.68 rating</span>
-        </li>
+        <?php foreach( $list as $anime ): ?>
+            <li class="item">
+                <p>
+                    <!-- TODO get endpoint config from class -->
+                    <a href="https://kitsu.io/anime/<?php echo $anime['attributes']['slug'] ?>" target="_blank">
+                        <img class="kitsu-anime-list-image" src="<?php echo $anime['attributes']['posterImage']['tiny']; ?>" alt="<?php echo $anime['attributes']['canonicalTitle']; ?>" title="<?php echo $anime['attributes']['canonicalTitle']; ?>" />
+                    </a>
+                </p>
+                <a href="https://kitsu.io/anime/<?php echo $anime['attributes']['slug'] ?>" class="title" target="_blank"><?php echo $anime['attributes']['canonicalTitle']; ?></a>
+                <span><?php echo $anime['attributes']['subtype'] . ', ' . $anime['attributes']['episodeCount'] . ', ' . $anime['attributes']['status'] ; ?></span>
+                <span><?php echo $anime['attributes']['averageRating']; ?> rating</span>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </div>
 
