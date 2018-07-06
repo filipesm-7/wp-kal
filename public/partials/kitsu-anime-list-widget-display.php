b9da119f40149dcb69389c55080c76e237b6a571
Filipe Mendonça <filipesm.7@gmail.com>
2018-07-06 11:55:26 +0100

    Changed widget template and added css

--- public/partials/kitsu-anime-list-widget-display.php
+++ public/partials/kitsu-anime-list-widget-display.php
@@ -1,24 +1,28 @@
-<ul>
-    <li>
-        <p>
-            <a href="https://kitsu.io/anime/spice-and-wolf">
-                <img src="https://media.kitsu.io/anime/poster_images/7880/tiny.jpg?1418914006" alt="Spice and Wolf" />
-            </a>
-        </p>
-        <p>Title here</p>
-        <span>Episodes: 24</span>
-        <span>Type: Drama, Fantasy</span>
-        <span>Rating: 9.99</span>
-    </li>
-    <li>
-        <p>
-            <a href="https://kitsu.io/anime/spice-and-wolf">
-                <img src="https://media.kitsu.io/anime/poster_images/10877/tiny.jpg?1465429917" alt="Spice and Wolf" />
-            </a>
-        </p>
-        <p>Title here</p>
-        <span>Episodes: 24</span>
-        <span>Type: Drama, Fantasy</span>
-        <span>Rating: 9.99</span>
-    </li>
-</ul>
+<div class="kitsu-anime-list">
+    <h2 class="widget-title"><?php echo $instance['title']; ?></h2>
+
+    <ul>
+        <li class="item">
+            <p>
+                <a href="https://kitsu.io/anime/spice-and-wolf">
+                    <img class="kitsu-anime-list-image" src="https://media.kitsu.io/anime/poster_images/3936/tiny.jpg?1419000387" alt="Spice and Wolf" />
+                </a>
+            </p>
+            <a href="" class="title">Fullmetal Alchemist: Brotherhood</a>
+            <span>TV, 64 episodes, finished</span>
+            <span>87.02 rating</span>
+        </li>
+        <li class="item">
+            <p>
+                <a href="https://kitsu.io/anime/spice-and-wolf">
+                    <img class="kitsu-anime-list-image" src="https://media.kitsu.io/anime/poster_images/12268/tiny.jpg?1491106234" alt="Spice and Wolf" />
+                </a>
+            </p>
+
+            <a href="" class="title">My Hero Academia Season 2</a>
+            <span>TV, 25 episodes, finished</span>
+            <span>85.68 rating</span>
+        </li>
+    </ul>
+</div>
+
