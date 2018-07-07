37fab898b7aa9112d56c0a28380b8a16e5ec4478
Filipe Mendonça <filipesm.7@gmail.com>
2018-07-07 23:12:50 +0100

    Widget template updated to also display manga

--- public/partials/kitsu-api-list-widget-display.php
+++ public/partials/kitsu-api-list-widget-display.php
@@ -2,17 +2,17 @@
     <h2 class="widget-title"><?php echo $instance['title']; ?></h2>
 
     <ul>
-        <?php foreach( $list as $anime ): ?>
+        <?php foreach( $list as $item ): ?>
             <li class="item">
                 <p>
                     <!-- TODO get endpoint config from class -->
-                    <a href="https://kitsu.io/anime/<?php echo $anime['attributes']['slug'] ?>" target="_blank">
-                        <img class="kitsu-api-list-image" src="<?php echo $anime['attributes']['posterImage']['tiny']; ?>" alt="<?php echo $anime['attributes']['canonicalTitle']; ?>" title="<?php echo $anime['attributes']['canonicalTitle']; ?>" />
+                    <a href="https://kitsu.io/<?php echo $instance['search_type']; ?>/<?php echo $item['attributes']['slug'] ?>" target="_blank">
+                        <img class="kitsu-api-list-image" src="<?php echo $item['attributes']['posterImage']['tiny']; ?>" alt="<?php echo $item['attributes']['canonicalTitle']; ?>" title="<?php echo $item['attributes']['canonicalTitle']; ?>" />
                     </a>
                 </p>
-                <a href="https://kitsu.io/anime/<?php echo $anime['attributes']['slug'] ?>" class="title" target="_blank"><?php echo $anime['attributes']['canonicalTitle']; ?></a>
-                <span><?php echo $anime['attributes']['subtype'] . ', ' . $anime['attributes']['episodeCount'] . ', ' . $anime['attributes']['status'] ; ?></span>
-                <span><?php echo $anime['attributes']['averageRating']; ?> rating</span>
+                <a href="https://kitsu.io/<?php echo $instance['search_type']; ?>/<?php echo $item['attributes']['slug'] ?>" class="title" target="_blank"><?php echo $item['attributes']['canonicalTitle']; ?></a>
+                <span><?php echo $item['attributes']['subtype']; ?>, <?php echo ( $instance['search_type'] == "anime" ) ? $item['attributes']['episodeCount'] : $item['attributes']['volumeCount']; ?>, <?php echo $item['attributes']['status']; ?></span>
+                <span><?php echo $item['attributes']['averageRating']; ?> rating</span>
             </li>
         <?php endforeach; ?>
     </ul>
