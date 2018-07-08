136bf88fbbf124f34f7d2d1dd0700c30eb29d9ee
Filipe Mendonça <filipesm.7@gmail.com>
2018-07-08 22:13:19 +0100

    Removed title multiline and added padding between title and information

--- public/partials/kitsu-api-list-widget-display.php
+++ public/partials/kitsu-api-list-widget-display.php
@@ -7,17 +7,20 @@
         <?php foreach( $list as $item ): ?>
             <li class="item">
                 <p>
-                    <!-- TODO get endpoint config from class -->
                     <a href="https://kitsu.io/<?php echo $instance['search_type']; ?>/<?php echo $item['attributes']['slug'] ?>" target="_blank">
                         <img class="kitsu-api-list-image" src="<?php echo $item['attributes']['posterImage']['tiny']; ?>" alt="<?php echo $item['attributes']['canonicalTitle']; ?>" title="<?php echo $item['attributes']['canonicalTitle']; ?>" />
                     </a>
                 </p>
-                <a href="https://kitsu.io/<?php echo $instance['search_type']; ?>/<?php echo $item['attributes']['slug'] ?>" class="title" target="_blank"><?php echo $item['attributes']['canonicalTitle']; ?></a>
-                <span><?php echo $item['attributes']['subtype']; ?>,
-                    <?php echo ( !empty( $item['attributes'][$count_type] ) ) ? $item['attributes'][$count_type] : __( "n/a", $this->plugin_name ); ?>,
-                    <?php echo $item['attributes']['status']; ?>
-                </span>
-                <span><?php echo $item['attributes']['averageRating']; ?> <?php _e( 'rating', $this->plugin_name ) ?></span>
+                <a class="title" href="https://kitsu.io/<?php echo $instance['search_type']; ?>/<?php echo $item['attributes']['slug'] ?>" target="_blank"><?php echo $item['attributes']['canonicalTitle']; ?></a>
+
+                <div class="kitsu-item-information">
+                    <span><?php echo $item['attributes']['subtype']; ?>,
+                        <?php echo ( !empty( $item['attributes'][$count_type] ) ) ? $item['attributes'][$count_type] : __( "n/a", $this->plugin_name ); ?>,
+                        <?php echo $item['attributes']['status']; ?>
+                    </span>
+                    <span><?php echo $item['attributes']['averageRating']; ?> <?php _e( 'rating', $this->plugin_name ) ?></span>
+                </div>
+
             </li>
         <?php endforeach; ?>
     </ul>
